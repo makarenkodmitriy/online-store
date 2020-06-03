f43f155cd2d2d28ac30affd0282b05013a8cf569
makarenkodmitriy <dim_tuncan@ukr.net>
2020-06-03 11:51:18 +0300

    First program :)

---
+++ src/MyOnlineStore/Catalog.java
@@ -0,0 +1,16 @@
+package MyOnlineStore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog {
+    private List<Product> catalog = new ArrayList<>();
+
+    public void addProductToCatalog(Product product) {
+        catalog.add(product);
+    }
+
+    public List<Product> getCatalog() {
+        return this.catalog;
+    }
+}
